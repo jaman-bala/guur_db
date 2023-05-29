9be7e85c1603ef6a9900c319c3d3ffb084732c37
Jamanbala_it <ermekoffdastan@gmail.com>
2023-05-29 20:47:53 +0600

    StackedInline

--- backend/apps/product/admin.py
+++ backend/apps/product/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from backend.apps.product.models import Product
+from backend.apps.product.models import Product, ProductImage
+
+
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+    extra = 5
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines=[ProductImageInline]
     list_display = [
         'code',
         'title',
         'erp',
     ]
+
+
+# {% for p in products %}
+#     {% for i in p.images.all %}
+#     {% endfor %}
+#
+# {%endfor%}
--- backend/apps/product/models.py
+++ backend/apps/product/models.py
@@ -37,3 +37,9 @@ class Product(models.Model):
     def __str__(self):
         return str(self.code)
 
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to="products/images")
+
