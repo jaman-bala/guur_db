fb06f5dc461bbb28a0c17f8e09cc4336204c2c6a
Jamanbala_it <ermekoffdastan@gmail.com>
2023-07-10 10:24:42 +0600

    edit settings.py and secret.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import os
-from backend.config.secret import SECRET_TOKEN
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = SECRET_TOKEN
+SECRET_KEY = 'django-insecure-6dbozjbdg2t!7o8fow9a%1#-%6=$4#6_m5-amphrh@1v$3^ii&'
 
 
 DEBUG = True
