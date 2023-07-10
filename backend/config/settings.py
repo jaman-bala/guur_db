9d06bc07b18e0072aff63ae2e5483a84e05f3c67
Jamanbala_it <ermekoffdastan@gmail.com>
2023-07-10 11:35:34 +0600

    edit settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,13 +4,13 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'django-insecure-6dbozjbdg2t!7o8fow9a%1#-%6=$4#6_m5-amphrh@1v$3^ii&'
+SECRET_KEY = '6dbozjbdg2t!7o8fow9a%1#-%6=$4#6_m5-amphrh@1v$3^ii&'
 
 
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = ['http://10.11.11.7']
 
 # Application definition
 
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('pageadmin/', admin.site.urls),
     # path('', include('backend.apps.account.urls')),
     path('base/', include('backend.apps.manuldb.urls')),
     path('people/', include('backend.apps.people.urls')),
